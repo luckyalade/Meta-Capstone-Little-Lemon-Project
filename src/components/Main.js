bbe990ea7c4e2fd6ef540854838f1060b018e699
luckyalade <luckyalade309@gmail.com>
2023-11-06 17:41:50 +0100

    updated Testimonials component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,13 @@
 import Hero from "../components/Hero";
 import Highlights from "../components/Highlights";
+import Testimonials from "../components/Testimonials";
 
 function Main() {
   return (
     <div>
       <Hero />
       <Highlights />
+      <Testimonials />
     </div>
   );
 }
---
+++ src/components/Testimonials.js
@@ -0,0 +1,34 @@
+function Testimonials() {
+  return (
+    <div>
+      <div className="Review_Card">
+        <div className="James_Card">
+          <h3>Rating Stars</h3>
+          <img src="#" alt="customer pics" />
+          <h4>James</h4>
+          <p>Customer Review</p>
+        </div>
+        <div className="Aaliyah_Card">
+          <h3>Rating Stars</h3>
+          <img src="#" alt="customer pics" />
+          <h4>Aaliyah</h4>
+          <p>Customer Review</p>
+        </div>
+        <div className="Lee_Card">
+          <h3>Rating Stars</h3>
+          <img src="#" alt="customer pics" />
+          <h4>Lee</h4>
+          <p>Customer Review</p>
+        </div>
+        <div className="Martha_Card">
+          <h3>Rating Stars</h3>
+          <img src="#" alt="customer pics" />
+          <h4>Martha</h4>
+          <p>Customer Review</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Testimonials;
