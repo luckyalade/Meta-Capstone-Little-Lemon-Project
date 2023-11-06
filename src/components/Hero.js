7e4b2dc990ff3897ffdfd4923c155b8689c809bd
luckyalade <luckyalade309@gmail.com>
2023-11-06 17:21:34 +0100

    included the Main Component and Hero Sub-Component

---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+function Hero() {
+  return (
+    <div>
+      <h1>Little Lemon</h1>
+      <h3>Chicago</h3>
+      <button type="button">Reserve a table</button>
+      <img src="#" alt="heroImage" />
+    </div>
+  );
+}
+
+export default Hero;
---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import Hero from "../components/Hero";
+
+function Main() {
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
+}
+
+export default Main;
