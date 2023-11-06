7709a61ca27159fb10bc9edc528fb6f0d011b2f1
luckyalade <luckyalade309@gmail.com>
2023-11-06 17:32:23 +0100

    updated Highlights component

---
+++ src/components/Highlights.js
@@ -0,0 +1,45 @@
+function Highlights() {
+  return (
+    <div>
+      <h2>Specials</h2>
+      <button type="button">create menu</button>
+      <div className="card">
+        <div className="greek_salad_card">
+          <img src="#" alt="Greek Salad" />
+          <h3>Greek Salad</h3>
+          <p>$12.53</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+            tenetur iure doloremque, architecto facere ut veritatis quisquam in
+            iusto rerum.
+          </p>
+          <p>order a deliver +1</p>
+        </div>
+        <div className="bruschetta_card">
+          <img src="#" alt="Bruschetta" />
+          <h3>Bruschetta</h3>
+          <p>$32.85</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+            tenetur iure doloremque, architecto facere ut veritatis quisquam in
+            iusto rerum.
+          </p>
+          <p>order a deliver +1</p>
+        </div>
+        <div className="lemon_salad_card">
+          <img src="#" alt="Lemon Salad" />
+          <h3>Lemon Salad</h3>
+          <p>$14.21</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+            tenetur iure doloremque, architecto facere ut veritatis quisquam in
+            iusto rerum.
+          </p>
+          <p>order a deliver +1</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Highlights;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,11 @@
 import Hero from "../components/Hero";
+import Highlights from "../components/Highlights";
 
 function Main() {
   return (
     <div>
       <Hero />
+      <Highlights />
     </div>
   );
 }
