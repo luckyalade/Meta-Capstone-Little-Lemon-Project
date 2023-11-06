803c6312063ae6eea6228d81834c99ff4cb0a00f
luckyalade <luckyalade309@gmail.com>
2023-11-06 17:55:01 +0100

    updated Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,46 @@
+function Footer() {
+  return (
+    <div>
+      <img src="#" alt="Little Lemon Logo" />
+      <div className="Doormat_Navigation">
+        <h3>Doormat Navigation</h3>
+        <nav>
+          <ul>
+            <li>
+              <a href="https://luckyalade.github.io">Home</a>
+            </li>
+            <li>
+              <a href="https://luckyalade.github.io">About</a>
+            </li>
+            <li>
+              <a href="https://luckyalade.github.io">Menu</a>
+            </li>
+            <li>
+              <a href="https://luckyalade.github.io">Reservations</a>
+            </li>
+            <li>
+              <a href="https://luckyalade.github.io">Online Orders</a>
+            </li>
+            <li>
+              <a href="https://luckyalade.github.io">Login</a>
+            </li>
+          </ul>
+        </nav>
+        <div className="Contact">
+          <h3>Contacts</h3>
+          <p>Address</p>
+          <p>Phone Number</p>
+          <p>Email</p>
+        </div>
+        <div className="Social_Media_Links">
+          <h3>Social Media Links</h3>
+          <p>Facebook</p>
+          <p>Twitter</p>
+          <p>Instagram</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
