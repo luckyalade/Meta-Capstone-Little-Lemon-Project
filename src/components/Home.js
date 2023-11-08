5d50ef58f88c029d7805c7f9b0b86dd1beb240be
luckyalade <luckyalade309@gmail.com>
2023-11-08 17:29:56 +0100

    minor adjustments to the homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,25 @@
+import BlackChef from "../black-chef.jpg";
+
 function Home() {
   return (
-    <div>
-      <h1>Welcome to the Little Lemon Restaurant</h1>
+    <div
+      className="hero"
+      style={{ display: "flex", width: "80%", margin: "0px 10%" }}
+    >
+      <div className="hero_body" style={{ width: "50%" }}>
+        <h1>Little Lemon</h1>
+        <h3>Chicago</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
+          similique sapiente quas exercitationem, quibusdam laborum inventore
+          ipsam error sunt eos tempora excepturi, at itaque ea corrupti delectus
+          doloremque, vel maiores!
+        </p>
+        <button type="button">Reserve a table</button>
+      </div>
+      <div className="hero_img" style={{ width: "50%" }}>
+        <img src={BlackChef} alt="Black Chef" width={400} />
+      </div>
     </div>
   );
 }
