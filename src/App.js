5d7897282e3eb3eac99c99646c1cf6199cfc2f86
luckyalade <luckyalade309@gmail.com>
2023-11-09 04:08:54 +0100

    random experiments

--- src/App.js
+++ src/App.js
@@ -6,31 +6,66 @@ import Reservations from "./components/Reservations";
 import OnlineOrders from "./components/OnlineOrders";
 import Login from "./components/Login";
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import LittleLemonLogo from "./Little_Lemon_Logo.svg";
 
 function App() {
   return (
     <Router>
       <div>
         <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/menu">Menu</Link>
-            </li>
-            <li>
-              <Link to="/reservations">Reservations</Link>
-            </li>
-            <li>
-              <Link to="/onlineOrders">Online Orders</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
+          <ul
+            style={{
+              display: "flex",
+              listStyle: "none",
+              justifyContent: "space-evenly",
+              alignItems: "flex-end",
+              width: "80%",
+            }}
+          >
+            <img src={LittleLemonLogo} alt="" />
+            <div
+              style={{
+                width: "50%",
+                display: "flex",
+                justifyContent: "space-around",
+                alignItems: "flex-end",
+                position: "relative",
+                top: "-10px",
+                left: "100px",
+                float: "right",
+              }}
+            >
+              <li>
+                <Link to="/" id="link">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link to="/about" id="link">
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link to="/menu" id="link">
+                  Menu
+                </Link>
+              </li>
+              <li>
+                <Link to="/reservations" id="link">
+                  Reservations
+                </Link>
+              </li>
+              <li>
+                <Link to="/onlineOrders" id="link">
+                  Online Orders
+                </Link>
+              </li>
+              <li>
+                <Link to="/login" id="link">
+                  Login
+                </Link>
+              </li>
+            </div>
           </ul>
         </nav>
         <Routes>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,22 @@
-import BlackChef from "../black-chef.jpg";
+import BlackChef from "../black-chef-removebg-preview.png";
 
 function Home() {
   return (
     <div
       className="hero"
-      style={{ display: "flex", width: "80%", margin: "0px 10%" }}
+      style={{
+        display: "flex",
+        width: "80%",
+        margin: "0px 10%",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "80vh",
+      }}
     >
       <div className="hero_body" style={{ width: "50%" }}>
-        <h1>Little Lemon</h1>
+        <h1>
+          <span></span>Little Lemon
+        </h1>
         <h3>Chicago</h3>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
