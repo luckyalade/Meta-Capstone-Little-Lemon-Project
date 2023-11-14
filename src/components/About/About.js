99a16e685477a23a0e4fb2b9d6dd35709f1c2c3d
luckyalade <luckyalade309@gmail.com>
2023-11-14 18:35:22 +0100

    about us completed

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,13 +1,43 @@
+import "./About.css";
+import LemonSlices from "./lemon-slices-img.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPizzaSlice } from "@fortawesome/free-solid-svg-icons";
+
 export default function About() {
   return (
-    <div>
-      <h1>About the Little lemon Restaurant</h1>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta
-        deserunt, dolore in enim recusandae delectus dolorum sapiente minus
-        perferendis excepturi quae iste culpa, amet, atque assumenda. Nemo cum
-        deleniti consectetur.
-      </p>
+    <div className="about">
+      <div className="about-us">
+        <div>
+          <img src={LemonSlices} alt="" width="300px" id="about-img" />
+        </div>
+        <div>
+          <h2>About Us</h2>
+          <p>
+            Little Lemon, a Mediterranean gem, sprouted from the culinary
+            passion of two friends, Alessia and Marco. Established in 2010 in
+            the heart of Naples, their restaurant quickly became a local
+            favorite, blending traditional flavors with a modern twist. Inspired
+            by their travels across the Mediterranean, the duo crafted a menu
+            that celebrates the region's diverse culinary heritage. Fresh
+            ingredients, bold spices, and a warm ambiance define the Little
+            Lemon experience. Over the years, it has evolved into a cherished
+            culinary haven, inviting patrons to savor the essence of the
+            Mediterranean in every delectable bite.
+            <FontAwesomeIcon
+              icon={faPizzaSlice}
+              style={{
+                width: "200px",
+                height: "200px",
+                opacity: "0.1",
+                position: "absolute",
+                right: "300px",
+                bottom: "-200px",
+                color: "grey",
+              }}
+            />
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
