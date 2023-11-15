ca264bb7aa15bad068b6c60f7f1055bcf986e618
luckyalade <luckyalade309@gmail.com>
2023-11-15 16:48:44 +0100

    little adjustme reservation

--- src/components/Reservations/Reservations.js
+++ src/components/Reservations/Reservations.js
@@ -8,6 +8,7 @@ const ReservationForm = () => {
   const [guests, setGuests] = useState("");
   const [comments, setComments] = useState("");
   const [seatingPreference, setSeatingPreference] = useState("inside");
+  const [showModal, setShowModal] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -20,6 +21,20 @@ const ReservationForm = () => {
       comments,
       seatingPreference,
     });
+
+    // Show the modal after submission
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    // Close the modal and reset the form
+    setShowModal(false);
+    setName("");
+    setDate("");
+    setTime("");
+    setGuests("");
+    setComments("");
+    setSeatingPreference("inside");
   };
 
   return (
@@ -36,69 +51,61 @@ const ReservationForm = () => {
         </div>
         <div className="form">
           <form onSubmit={handleSubmit}>
-            <label>
-              Name:
-              <input
-                type="text"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                required
-              />
-            </label>
+            <label>Name:</label>
             <br />
-            <label>
-              Date:
-              <input
-                type="date"
-                value={date}
-                onChange={(e) => setDate(e.target.value)}
-                required
-              />
-            </label>
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
             <br />
-            <label>
-              Time:
-              <input
-                type="time"
-                value={time}
-                onChange={(e) => setTime(e.target.value)}
-                required
-              />
-            </label>
+            <label>Date:</label>
             <br />
-            <label>
-              Number of Guests:
-              <input
-                type="number"
-                value={guests}
-                onChange={(e) => setGuests(e.target.value)}
-                required
-              />
-            </label>
+            <input
+              type="date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+              required
+            />
             <br />
-            <label>
-              Seating Preference:
-              <select
-                value={seatingPreference}
-                onChange={(e) => setSeatingPreference(e.target.value)}
-              >
-                <option value="inside">Inside</option>
-                <option value="outside">Outside</option>
-              </select>
-            </label>
             <br />
-            <label>
-              Additional Comments:
-              <textarea
-                value={comments}
-                onChange={(e) => setComments(e.target.value)}
-              ></textarea>
-            </label>
+            <label>Number of Guests:</label>
+            <br />
+            <input
+              type="number"
+              value={guests}
+              onChange={(e) => setGuests(e.target.value)}
+              required
+            />
+            <br />
+            <label>Seating Preference:</label>
+            <br />
+            <select
+              value={seatingPreference}
+              onChange={(e) => setSeatingPreference(e.target.value)}
+            >
+              <option value="inside">Inside</option>
+              <option value="outside">Outside</option>
+            </select>
             <br />
             <button type="submit">Reserve Table</button>
           </form>
         </div>
       </div>
+
+      {/* Modal */}
+      {showModal && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={closeModal}>
+              &times;
+            </span>
+            <p>Thank you for your reservation!</p>
+            {/* You can customize the content of the modal as needed */}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
