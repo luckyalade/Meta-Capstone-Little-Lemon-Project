f85906c150c42f2fa2bc912de9b8458c66a76703
luckyalade <luckyalade309@gmail.com>
2023-11-16 13:59:02 +0100

    newly updated footer

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,6 +1,12 @@
 import React from "react";
 import "./ContactStyle.css";
 import FooterLogo from "./Images/Little_Lemon_Logo.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faAddressBook,
+  faEnvelope,
+  faHome,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function Login() {
   return (
@@ -20,12 +26,28 @@ export default function Login() {
         </div>
         <div className="contact-navigation">
           <h3>Contact</h3>
-          <p>Address</p>
-          <p>Phone Number</p>
-          <p>Email</p>
+          <p>
+            <span>
+              <FontAwesomeIcon icon={faHome} />
+            </span>
+            {"  "}
+            Boulevard Crescent, <br />
+            Little Lemon HQ.
+          </p>
+          <p>
+            <FontAwesomeIcon icon={faAddressBook} />
+            <span> +123123123</span>
+          </p>
+          <p>
+            <span>
+              <FontAwesomeIcon icon={faEnvelope} />
+            </span>
+            {"  "}
+            order@littlelemon.com
+          </p>
         </div>
         <div className="social-media-navigation">
-          <h3>Socila Media Links</h3>
+          <h3>Social Media Links</h3>
           <p>Facebook</p>
           <p>Instagram</p>
           <p>Twitter</p>
