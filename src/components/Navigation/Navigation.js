beb9456e16032b5d0d03411b70c78ff264a1d70f
luckyalade <luckyalade309@gmail.com>
2023-11-18 08:26:51 +0100

    random fixes

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -29,7 +29,7 @@ const Navigation = () => {
       style={{
         position: isScrolled ? "sticky" : "relative",
         top: 0,
-        backgroundColor: isScrolled ? "#333" : "transparent",
+        backgroundColor: isScrolled ? "rgba(0,0,0,0.5)" : "transparent",
         boxShadow: isScrolled ? "0px 2px 5px rgba(0, 0, 0, 0.1)" : "none",
         backdropFilter: isScrolled ? "blur(10px)" : "none",
         padding: "1px 0",
